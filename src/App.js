3829124637f144f931f973d6e38a7048908b2845
abhijeetr-ajmerainfotech <abhijeet.rajput@ajmerainfotech.com>
2023-01-27 17:39:09 +0530

    added missing package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import UserComponent from "./components/UserComponent";
-
+import React from "react";
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./redux/store";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.unstable_createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
